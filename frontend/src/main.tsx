cd07171b5003eb7f0c5820fb2f0c472d955f52d5
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-22 19:02:14 +0600

    fix: change Toaster position to top-center for better visibility

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
   <Provider store={store}>
-   <Toaster position="top-right" richColors />
+   <Toaster position="top-center" richColors />
    <Todo />
   </Provider>
  </StrictMode>,
