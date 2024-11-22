edd733da4cbbdedc448b4e9b9108bbb7f763f12c
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-22 19:03:56 +0600

    feat: update Container component to support responsive height adjustments

--- frontend/src/components/ui/container.tsx
+++ frontend/src/components/ui/container.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 const Container = ({ children }: PropsWithChildren) => {
  return (
-  <section className="min-h-[90vh] w-full max-w-screen-xl mx-auto px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
+  <section className="min-h-[90dvh] sm:min-h-[90svh] md:min-h-[90vh] lg:min-h-[90lvh] xl:min-h-[90lvh] 2xl:min-h-[90lvh] w-full max-w-screen-xl mx-auto px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
    {children}
   </section>
  );
