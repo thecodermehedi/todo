f7d9b5caf61ac7e4456803b1d5cac2a7f6d16ad9
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-22 19:04:01 +0600

    feat: enhance API to support priority filtering and add update/remove todo mutations

--- frontend/src/redux/api/api.ts
+++ frontend/src/redux/api/api.ts
@@ -3,14 +3,31 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
  reducerPath: 'baseApi',
  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+ tagTypes: ["tasks"],
  endpoints: (builder) => ({
   getTodos: builder.query({
-   query: () => ({ url: "/tasks", method: "GET" })
+   query: (priority) => {
+    const params = new URLSearchParams()
+    if (priority) {
+     params.append('priority', priority)
+    }
+    return { url: "/tasks", method: "GET", params }
+   },
+   providesTags: ["tasks"]
   }),
   addTodo: builder.mutation({
-   query: (data) => ({ url: "/tasks", method: "POST", body: data })
+   query: (data) => ({ url: "/tasks", method: "POST", body: data }),
+   invalidatesTags: ["tasks"]
+  }),
+  updateTodo: builder.mutation({
+   query: (options) => ({ url: `/task/${options.id}`, method: "PUT", body: options.data }),
+   invalidatesTags: ["tasks"]
+  }),
+  removeTodo: builder.mutation({
+   query: (id) => ({ url: `/task/${id}`, method: "DELETE" }),
+   invalidatesTags: ["tasks"]
   })
  }),
 })
 
-export const { useGetTodosQuery, useAddTodoMutation } = baseApi
+export const { useGetTodosQuery, useAddTodoMutation, useUpdateTodoMutation, useRemoveTodoMutation } = baseApi
